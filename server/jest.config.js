25cd2eab981a31c26ea822dffcea9a0ad0e5f207
Fikre-M <fikreddu@gmail.com>
2026-02-26 08:36:45 -0500

    Delate unrelated test files

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,61 +1,27 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  testMatch: ["**/__tests__/**/*.test.ts"],
-  collectCoverageFrom: [
-    "src/**/*.ts",
-    "!src/**/*.d.ts",
-    "!src/**/__tests__/**",
-    "!src/types/**",
-    "!src/config/**",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.ts',
+    '**/?(*.)+(spec|test).ts'
   ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-    "src/services/AuthService.ts": {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
-    },
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
   },
-  coverageDirectory: "coverage",
-  verbose: true,
-  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/types/**',
+    '!src/scripts/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    'text',
+    'lcov',
+    'html'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+  testTimeout: 30000,
+  maxWorkers: 1, // Run tests sequentially to avoid database conflicts
 };
-
-// module.exports = {
-//   preset: 'ts-jest',
-//   testEnvironment: 'node',
-//   roots: ['<rootDir>/src'],
-//   testMatch: [
-//     '**/__tests__/**/*.ts',
-//     '**/?(*.)+(spec|test).ts'
-//   ],
-//   transform: {
-//     '^.+\\.ts$': 'ts-jest',
-//   },
-//   collectCoverageFrom: [
-//     'src/**/*.ts',
-//     '!src/**/*.d.ts',
-//     '!src/types/**',
-//     '!src/scripts/**',
-//   ],
-//   coverageDirectory: 'coverage',
-//   coverageReporters: [
-//     'text',
-//     'lcov',
-//     'html'
-//   ],
-//   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
-//   testTimeout: 30000,
-//   maxWorkers: 1, // Run tests sequentially to avoid database conflicts
-// };
